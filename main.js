05ca0789320e16a4ccc996bd311ce4f332bfd677
husnacoding <husnaamalia96@gmail.com>
2025-03-18 21:39:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    }
+    )
+}
+
+window.onbeforeunload= () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
